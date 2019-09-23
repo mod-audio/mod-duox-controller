7fda0b07d24457f2fae9012def27b9bfb3c06964
Jan <jan@moddevices.com>
2019-09-23 12:39:02 +0200

    changed displday brightness PWM + redo LED time calculation

--- drivers/inc/ledz.h
+++ drivers/inc/ledz.h
@@ -75,7 +75,7 @@ extern "C"
 //#define LEDZ_BRIGHTNESS_SUPPORT
 
 // tick period in us
-#define LEDZ_TICK_PERIOD        10
+#define LEDZ_TICK_PERIOD        5
 
 //always blink value
 #define LED_BLINK_INFINIT		-1
