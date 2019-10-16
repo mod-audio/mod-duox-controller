64d5c4c4e9cc1d96e0f3a3355b3920393a05b98f
Jan <jan@moddevices.com>
2019-10-16 14:36:24 +0200

    make sure to not use hardcoded value for led tick time

--- drivers/inc/ledz.h
+++ drivers/inc/ledz.h
@@ -75,7 +75,7 @@ extern "C"
 //#define LEDZ_BRIGHTNESS_SUPPORT
 
 // tick period in us
-#define LEDZ_TICK_PERIOD        100
+#define LEDZ_TICK_PERIOD        LED_DISPL_INTERUPT_TIME
 
 //always blink value
 #define LED_BLINK_INFINIT		-1
