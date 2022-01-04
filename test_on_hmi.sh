f310c8446d879b11e6a4852f50f884881cd19fd6
Jan <jan@moddevices.com>
2022-01-04 18:39:45 +0100

    clean bin files after deploy

--- test_on_hmi.sh
+++ test_on_hmi.sh
@@ -9,3 +9,5 @@ sshpass -p "mod" scp ./out/mod-duox-controller.bin root@192.168.51.1:/tmp/
 sshpass -p 'mod' ssh root@192.168.51.1 hmi-update /tmp/mod-duox-controller.bin
 
 sshpass -p 'mod' ssh root@192.168.51.1 systemctl restart mod-ui
+
+make clean
